a061844529f241fbd0e0ef394b2fa70ac0e17d90
CHAEWAN KIM <66085474+amsminn@users.noreply.github.com>
2025-06-21 22:53:45 +0900

    [SWM-127] feat: init empty user-accounts, user-stats repository

---
+++ src/main/java/com/climbx/climbx/user/repository/UserAccountRepository.java
@@ -0,0 +1,9 @@
+package com.climbx.climbx.user.repository;
+
+import com.climbx.climbx.user.entity.UserAccount;
+import java.math.BigInteger;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserAccountRepository extends JpaRepository<UserAccount, BigInteger> {
+
+}
