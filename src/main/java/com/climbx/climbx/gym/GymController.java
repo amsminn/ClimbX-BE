6bbbbf5236a5d9ff3d1cc0b1ea81bda31cbd4efe
5win <woozzoo7@gmail.com>
2025-06-26 10:45:05 +0900

    [SWM-68] fix: @Validated를 추가하여 반환값 검증이 동작도록 수정

--- src/main/java/com/climbx/climbx/gym/GymController.java
+++ src/main/java/com/climbx/climbx/gym/GymController.java
@@ -4,6 +4,7 @@ import com.climbx.climbx.gym.dto.GymInfoResponseDto;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/gyms")
+@Validated
 @RequiredArgsConstructor
 public class GymController {
 
