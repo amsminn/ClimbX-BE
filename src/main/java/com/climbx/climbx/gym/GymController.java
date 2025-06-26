92a32a9b792f6ec6b6da6d9eed52e04466de5193
5win <woozzoo7@gmail.com>
2025-06-26 18:27:23 +0900

    [SWM-89] feat: 클라이밍장 상세 조회 기능 구현

--- src/main/java/com/climbx/climbx/gym/GymController.java
+++ src/main/java/com/climbx/climbx/gym/GymController.java
@@ -4,10 +4,13 @@ import com.climbx.climbx.gym.dto.GymInfoResponseDto;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +22,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class GymController {
 
     private final GymService gymService;
+
+    @GetMapping("/{gymId}")
+    public @Valid GymInfoResponseDto getGymById(@PathVariable @NotNull @Min(1L) Long gymId) {
+        return gymService.getGymById(gymId);
+    }
 
     @GetMapping(params = {"!latitude", "!longitude"})
     public List<@Valid GymInfoResponseDto> getGymList(
