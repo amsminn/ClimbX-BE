e6df735e3ebece0541a7e3b81a14940a221ccabb
5win <woozzoo7@gmail.com>
2025-06-29 16:21:28 +0900

    [SWM-136] docs: 주석 위치 이동

--- src/main/java/com/climbx/climbx/user/repository/UserAccountRepository.java
+++ src/main/java/com/climbx/climbx/user/repository/UserAccountRepository.java
@@ -40,12 +40,12 @@ public interface UserAccountRepository extends JpaRepository<UserAccountEntity, 
     // 특정 역할의 사용자들 조회
     List<UserAccountEntity> findByRole(RoleType role);
 
+    // 특정 역할의 모든 사용자 조회
     Page<UserAccountEntity> findByRole(RoleType role, Pageable pageable);
 
     // 닉네임에 특정 문자열을 포함하는 사용자들 조회 (List 반환)
     List<UserAccountEntity> findByNicknameContaining(String nickname);
 
-    // 특정 역할의 모든 사용자 조회
 
     // 특정 역할이면서 닉네임에 특정 문자열을 포함하는 사용자들 조회
     List<UserAccountEntity> findByRoleAndNicknameContaining(RoleType role, String nickname);
