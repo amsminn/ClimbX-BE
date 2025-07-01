1a3c63a4beffb68c9f987f36cbd34610b5afb450
amsminn <kim1109123@gmail.com>
2025-07-01 23:17:08 +0900

    [SWM-111] chore: Jaskson Instant 직렬화 포맷

--- src/main/java/com/climbx/climbx/common/dto/ApiResponseDto.java
+++ src/main/java/com/climbx/climbx/common/dto/ApiResponseDto.java
@@ -1,6 +1,7 @@
 package com.climbx.climbx.common.dto;
 
 import com.climbx.climbx.common.util.TimeContext;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.Instant;
 import lombok.Builder;
 import org.springframework.http.HttpStatus;
@@ -8,8 +9,14 @@ import org.springframework.http.HttpStatus;
 @Builder
 public record ApiResponseDto<T>(
 
+    @JsonFormat(shape = JsonFormat.Shape.NUMBER_INT)
     HttpStatus httpStatus,
 
+    @JsonFormat(
+        shape = JsonFormat.Shape.STRING,
+        pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'",           // ISO-8601 UTC 포맷 (밀리초까지, 끝에 Z로 UTC 표시)
+        timezone = "UTC"                                     // 입력·출력 시 UTC 타임존 기준으로 변환
+    )
     String statusMessage,
 
     Instant timeStamp,
