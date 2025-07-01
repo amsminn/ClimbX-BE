13c1d99116ecc6518f7312da2c1d28d90d854be7
amsminn <kim1109123@gmail.com>
2025-07-01 23:21:31 +0900

    [SWM-111] refactor: HttpStatus 타입 변경

--- src/main/java/com/climbx/climbx/common/dto/ApiResponseDto.java
+++ src/main/java/com/climbx/climbx/common/dto/ApiResponseDto.java
@@ -1,7 +1,6 @@
 package com.climbx.climbx.common.dto;
 
 import com.climbx.climbx.common.util.TimeContext;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.Instant;
 import lombok.Builder;
 import org.springframework.http.HttpStatus;
@@ -9,14 +8,8 @@ import org.springframework.http.HttpStatus;
 @Builder
 public record ApiResponseDto<T>(
 
-    @JsonFormat(shape = JsonFormat.Shape.NUMBER_INT)
-    HttpStatus httpStatus,
+    Long httpStatus,
 
-    @JsonFormat(
-        shape = JsonFormat.Shape.STRING,
-        pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'",           // ISO-8601 UTC 포맷 (밀리초까지, 끝에 Z로 UTC 표시)
-        timezone = "UTC"                                     // 입력·출력 시 UTC 타임존 기준으로 변환
-    )
     String statusMessage,
 
     Instant timeStamp,
@@ -33,7 +26,7 @@ public record ApiResponseDto<T>(
          * success
          */
         return ApiResponseDto.<T>builder()
-            .httpStatus(HttpStatus.OK)
+            .httpStatus((long) HttpStatus.OK.value())
             .statusMessage("SUCCESS")
             .timeStamp(Instant.now())
             .responseTimeMs(TimeContext.getResponseTime())
@@ -47,7 +40,7 @@ public record ApiResponseDto<T>(
          * success
          */
         return ApiResponseDto.<T>builder()
-            .httpStatus(httpStatus)
+            .httpStatus((long) httpStatus.value())
             .statusMessage("SUCCESS")
             .timeStamp(Instant.now())
             .responseTimeMs(TimeContext.getResponseTime())
@@ -61,7 +54,7 @@ public record ApiResponseDto<T>(
          * success with custom message
          */
         return ApiResponseDto.<T>builder()
-            .httpStatus(HttpStatus.OK)
+            .httpStatus((long) HttpStatus.OK.value())
             .statusMessage(message)
             .timeStamp(Instant.now())
             .responseTimeMs(TimeContext.getResponseTime())
@@ -76,7 +69,7 @@ public record ApiResponseDto<T>(
          * error
          */
         return ApiResponseDto.<T>builder()
-            .httpStatus(httpStatus)
+            .httpStatus((long) httpStatus.value())
             .statusMessage(message)
             .timeStamp(Instant.now())
             .responseTimeMs(TimeContext.getResponseTime())
