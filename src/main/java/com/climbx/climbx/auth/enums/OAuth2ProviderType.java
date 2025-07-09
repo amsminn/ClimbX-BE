2b9e7738c95ecab942fe6117e8f310d64388fdd5
amsminn <kim1109123@gmail.com>
2025-07-09 17:02:34 +0900

    [SWM-130] refact: remove unused field from OAuth2ProviderType

--- src/main/java/com/climbx/climbx/auth/enums/OAuth2ProviderType.java
+++ src/main/java/com/climbx/climbx/auth/enums/OAuth2ProviderType.java
@@ -1,15 +1,12 @@
 package com.climbx.climbx.auth.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum OAuth2ProviderType {
-    KAKAO("카카오"),
-    GOOGLE("구글"),
-    APPLE("애플");
-
-    private final String provider;
-
-    OAuth2ProviderType(String displayName) {
-        this.provider = displayName;
-    }
+    KAKAO,
+    GOOGLE,
+    APPLE;
 
     public static OAuth2ProviderType fromString(String provider) {
         try {
@@ -18,8 +15,4 @@ public enum OAuth2ProviderType {
             throw new IllegalArgumentException("지원하지 않는 OAuth2 제공자입니다: " + provider);
         }
     }
-
-    public String getProvider() {
-        return provider;
-    }
-} 
+}
