a4ce455e0357c650b829ce1119104ff51553992b
amsminn <kim1109123@gmail.com>
2025-07-09 16:48:53 +0900

    [SWM-130] refact: JsonNaming annotation applied

--- src/main/java/com/climbx/climbx/auth/provider/kakao/dto/KakaoTokenResponseDto.java
+++ src/main/java/com/climbx/climbx/auth/provider/kakao/dto/KakaoTokenResponseDto.java
@@ -1,27 +1,17 @@
 package com.climbx.climbx.auth.provider.kakao.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record KakaoTokenResponseDto(
-    @JsonProperty("token_type")
+
     String tokenType,
-
-    @JsonProperty("access_token")
     String accessToken,
-
-    @JsonProperty("id_token")
     String idToken, // OpenID Connect ID 토큰
-
-    @JsonProperty("expires_in")
     Integer expiresIn,
-
-    @JsonProperty("refresh_token")
     String refreshToken,
-
-    @JsonProperty("refresh_token_expires_in")
     Integer refreshTokenExpiresIn,
-
-    @JsonProperty("scope")
     String scope
 ) {
 
