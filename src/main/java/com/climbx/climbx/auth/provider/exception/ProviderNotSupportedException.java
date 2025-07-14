7c6bad98479a0409c7f50384392904ecd3dee15e
CHAEWAN KIM <66085474+amsminn@users.noreply.github.com>
2025-07-14 17:12:35 +0900

    [SWM-146] refact: fix typo "ProviderNotSupportException" to "ProviderNotSupportedException"

--- src/main/java/com/climbx/climbx/auth/provider/exception/ProviderNotSupportedException.java
+++ src/main/java/com/climbx/climbx/auth/provider/exception/ProviderNotSupportedException.java
@@ -4,9 +4,9 @@ import com.climbx.climbx.auth.enums.OAuth2ProviderType;
 import com.climbx.climbx.common.error.BusinessException;
 import com.climbx.climbx.common.error.ErrorCode;
 
-public class ProviderNotSupportException extends BusinessException {
+public class ProviderNotSupportedException extends BusinessException {
 
-    public ProviderNotSupportException(OAuth2ProviderType provider) {
+    public ProviderNotSupportedException(OAuth2ProviderType provider) {
         super(ErrorCode.PROVIDER_NOT_SUPPORTED);
         addContext("provider", provider.name());
     }
