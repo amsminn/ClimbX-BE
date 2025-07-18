5aeb15d5cb47b9d0669f8e1ea94a4a267ffff93e
amsminn <kim1109123@gmail.com>
2025-07-18 17:59:36 +0900

    [SWM-161] feat: 랭킹 레포지토리 및 정렬 기준 처리

---
+++ src/main/java/com/climbx/climbx/ranking/repository/RankingRepository.java
@@ -0,0 +1,17 @@
+package com.climbx.climbx.ranking.repository;
+
+import com.climbx.climbx.user.entity.UserStatEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface RankingRepository extends JpaRepository<UserStatEntity, Long> {
+
+    @Query("""
+        SELECT u FROM UserStatEntity u
+           JOIN FETCH u.userAccountEntity ua
+           WHERE ua.role = :role
+        """)
+    Page<UserStatEntity> findAllByUserRole(Pageable pageable, String role);
+}
