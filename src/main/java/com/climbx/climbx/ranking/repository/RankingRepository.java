4aa546f4de942e9564f293fbee2b6b360c5bb2a9
amsminn <kim1109123@gmail.com>
2025-07-18 18:16:07 +0900

    [SWM-161] test: RankingService 유닛 테스트 작성

--- src/main/java/com/climbx/climbx/ranking/repository/RankingRepository.java
+++ src/main/java/com/climbx/climbx/ranking/repository/RankingRepository.java
@@ -5,13 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface RankingRepository extends JpaRepository<UserStatEntity, Long> {
 
-    @Query("""
-        SELECT u FROM UserStatEntity u
-           JOIN FETCH u.userAccountEntity ua
-           WHERE ua.role = :role
-        """)
-    Page<UserStatEntity> findAllByUserRole(Pageable pageable, String role);
+    @Query("SELECT u FROM UserStatEntity u " +
+           "JOIN FETCH u.userAccountEntity ua " +
+           "WHERE ua.role = :role")
+    Page<UserStatEntity> findAllByUserRole(Pageable pageable, @Param("role") String role);
 }
