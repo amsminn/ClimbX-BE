80d45c89aa6e52b3c5dd9644ef04e449ea4db2b6
amsminn <kim1109123@gmail.com>
2025-07-30 01:45:57 +0900

    [SWM-157] feat: SoftDeleteTimeEntity @Accessors(fluent = true) 활성화

--- src/main/java/com/climbx/climbx/common/entity/SoftDeleteTimeEntity.java
+++ src/main/java/com/climbx/climbx/common/entity/SoftDeleteTimeEntity.java
@@ -6,11 +6,13 @@ import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED) // 기본 생성자는 protected로 설정
+@Accessors(fluent = true)
 @Getter
 public abstract class SoftDeleteTimeEntity {
 
