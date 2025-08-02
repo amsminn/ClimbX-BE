517e3788524d0e269fdf6f6c0ee3a883cb66b771
CHAEWAN KIM <66085474+amsminn@users.noreply.github.com>
2025-08-02 21:13:00 +0900

    [SWM-193] feat: CallbackRequestDto validation

--- src/main/java/com/climbx/climbx/auth/dto/CallbackRequestDto.java
+++ src/main/java/com/climbx/climbx/auth/dto/CallbackRequestDto.java
@@ -1,12 +1,15 @@
 package com.climbx.climbx.auth.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 
 @Builder
 public record CallbackRequestDto(
 
+    @NotBlank
     String idToken, // OpenID 토큰
-    String nonce // replay attack 방지를 위한 nonce 값
+
+    String nonce // replay attack 방지를 위한 nonce 값, nullable
 ) {
 
 }
