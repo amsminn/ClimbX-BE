86dd2239716811f9e6c0697abea3233cf293214a
5win <woozzoo7@gmail.com>
2025-08-02 18:29:49 +0900

    [SWM-232] refact: 유저 정보 수정 validation 메시지 제거

--- src/main/java/com/climbx/climbx/user/dto/UserProfileModifyRequestDto.java
+++ src/main/java/com/climbx/climbx/user/dto/UserProfileModifyRequestDto.java
@@ -6,10 +6,10 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Builder
 public record UserProfileModifyRequestDto(
-    @Size(min = 1, max = 64, message = "닉네임은 최대 64자까지 입력 가능합니다.")
+    @Size(min = 1, max = 64)
     String newNickname,
 
-    @Size(max = 128, message = "상태 메시지는 최대 128 입력 가능합니다.")
+    @Size(max = 128)
     String newStatusMessage,
 
     MultipartFile profileImage // null 허용
