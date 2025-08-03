e713ce3c9fc0a9638d08929d8f992c5bfd26e954
CHAEWAN KIM <66085474+amsminn@users.noreply.github.com>
2025-08-04 04:43:12 +0900

    [SWM-155] refact: primary/secondary Tag Type fix

--- src/main/java/com/climbx/climbx/problem/entity/ProblemEntity.java
+++ src/main/java/com/climbx/climbx/problem/entity/ProblemEntity.java
@@ -3,6 +3,7 @@ package com.climbx.climbx.problem.entity;
 import com.climbx.climbx.common.entity.BaseTimeEntity;
 import com.climbx.climbx.common.enums.ActiveStatusType;
 import com.climbx.climbx.gym.entity.GymEntity;
+import com.climbx.climbx.problem.enums.ProblemType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -56,13 +57,11 @@ public class ProblemEntity extends BaseTimeEntity {
     @Max(value = 30)
     private Integer problemRating; // 문제 난이도
 
-    @Column(name = "primary_tag", length = 32)
-    @Size(max = 32) // nullable
-    private String primaryTag;
+    @Column(name = "primary_tag", length = 16)
+    private ProblemType primaryTag;
 
-    @Column(name = "secondary_tag", length = 32)
-    @Size(max = 32) // nullable
-    private String secondaryTag;
+    @Column(name = "secondary_tag", length = 16)
+    private ProblemType secondaryTag;
 
     @Column(name = "problem_image_cdn_url", length = 512)
     @Size(max = 512)
